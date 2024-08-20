14c3425f9b4af101d2f83887fe868fbbfd563c9f
oztrkmrt <merttoztrk@gmail.com>
2024-08-20 17:44:45 +0300

    first commit

---
+++ src/components/Header/HeaderMenuItem.jsx
@@ -0,0 +1,12 @@
+const HeaderMenuItem = () => {
+    return (
+        <div className='flex flex-col justify-between text-3xl py-20 justify-center items-center'>
+            <p className="py-4 text-[#737373]">Home</p>
+            <p className="py-4 text-[#737373]">Product</p>
+            <p className="py-4 text-[#737373]">Pricing</p>
+            <p className="py-4 text-[#737373]">Contact</p>
+        </div>
+    )
+}
+
+export default HeaderMenuItem;
---
+++ src/components/Header/HeaderNavBar.jsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons'
+
+const HeaderNavBar = () => {
+    return (
+        <div className='flex justify-between'>
+            <span className="font-bold text-2xl px-8 py-6 text-[#252B42] ">Brand</span>
+            <div className='flex items-center px-8 py-6 justify-between'>
+                <FontAwesomeIcon className='px-4' href='#' icon={faMagnifyingGlass} />
+                <FontAwesomeIcon className='px-4' icon={faCartShopping} />
+                <FontAwesomeIcon className='px-4' icon={faBars} />
+            </div>
+        </div>
+    )
+}
+
+export default HeaderNavBar
