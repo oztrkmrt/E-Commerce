cbe93b4d4e7d0c83a723cb5a1ae45516134d9066
oztrkmrt <merttoztrk@gmail.com>
2024-08-20 17:53:03 +0300

    T2: Home Page Fixes #3

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,13 @@
+import HeaderMenuItem from "@/components/Header/HeaderMenuItem";
+import HeaderNavBar from "@/components/Header/HeaderNavBar";
+import HeroCarousel from "@/components/Header/HeroCarousel";
+
 const Header = () => {
     return (
         <div>
-
+            <HeaderNavBar />
+            <HeaderMenuItem />
+            <HeroCarousel />
         </div>
     );
 };
