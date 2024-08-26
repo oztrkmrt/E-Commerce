4b0139d6f598f92f1200e17f8c73f662f0bce326
oztrkmrt <merttoztrk@gmail.com>
2024-08-26 21:41:52 +0300

    Started T3

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,14 +1,14 @@
 
 import HomePage from "@/pages/HomePage";
+import ShopPage from "@/pages/ShopPage";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
 
 const PageContent = () => {
     return (
         <div>
             <Switch>
-                <Route path="/" exact>
-                    <HomePage />
-                </Route>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/shop" component={ShopPage} />
             </Switch>
         </div>
     );
